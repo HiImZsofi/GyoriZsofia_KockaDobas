8cc652c93a59c8fd940522a179808d633f51675a
HiImZsofi <zsofia.gyori00@gmail.com>
2022-10-14 14:53:14 +0200

    initialize variables

--- app/src/main/java/com/example/kockadobas/MainActivity.kt
+++ app/src/main/java/com/example/kockadobas/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.kockadobas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rlayout : RelativeLayout
+    private lateinit var lnlayout : LinearLayout
+    private lateinit var dicelayout : LinearLayout
+    private lateinit var right : ImageView
+    private lateinit var left : ImageView
+    private lateinit var diceone : Button
+    private lateinit var dicetwo : Button
+    private lateinit var btnThrow : Button
+    private lateinit var btnReset : Button
+    private lateinit var numbers : TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    fun init(){
+        rlayout = findViewById(R.id.rlayout)
+        lnlayout = findViewById(R.id.lnlayout)
+        dicelayout = findViewById(R.id.dicelayout)
+        right = findViewById(R.id.right)
+        left = findViewById(R.id.left)
+        diceone = findViewById(R.id.diceone)
+        dicetwo = findViewById(R.id.dicetwo)
+        btnThrow = findViewById(R.id.btnThrow)
+        btnReset = findViewById(R.id.btnReset)
+        numbers = findViewById(R.id.numbers)
     }
 }
